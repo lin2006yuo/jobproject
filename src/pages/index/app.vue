7c8e3908dea2e62cc169a71e9f7fcbc9c879bbe9
lin2006yuo <547677504@qq.com>
2018-10-21 17:19:13 +0800

    '1.0'

---
+++ src/pages/index/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="index">
+        index
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    data() {
+        return {};
+    },
+    components: {}
+};
+</script>
+
+<style>
+</style>
